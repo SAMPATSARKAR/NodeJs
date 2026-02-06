378371220e9e7c6cbfc9f6ea5586c9fc1ee3c74c
Sampat <sampat@Sampats-MacBook-Air.local>
2026-02-07 01:04:03 +0530

    Completed EventEmitter

---
+++ 02_EventModules_EventClass/app.js
@@ -0,0 +1,38 @@
+const EventEmitter = require('events');
+const eventEmitter = new EventEmitter();
+
+eventEmitter.on('tutorial',(num1,num2)=>{
+    console.log(num1 +num2);
+    
+});
+
+eventEmitter.emit('tutorial',1,2); 
+
+class Person extends EventEmitter{// now class Person has eventEmitter powers
+    constructor(name){
+        super();
+        this._name = name;
+    }
+
+    get name(){
+        return this._name;
+    }
+
+
+
+}
+
+
+let pedro = new Person('Pedro');// now pedro has name and eventemitter powers
+let christina = new Person('Christina');
+christina.on('name',()=>{
+    console.log('My name is ' + christina.name);
+    
+})
+pedro.on('name',()=>{
+    console.log("My name is " + pedro.name);
+    
+    })
+
+pedro.emit('name');
+christina.emit('name');
