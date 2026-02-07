039eb7593e2a22697d87326c74fd6c6d7fb2b37d
Sampat <sampat@Sampats-MacBook-Air.local>
2026-02-08 03:05:38 +0530

    Completed reading lines from a file

---
+++ 03_ReadLineModule/app.js
@@ -0,0 +1,31 @@
+const readline = require('readline');
+const rl = readline.createInterface({input : process.stdin,
+                          output: process.stdout})
+
+let num1 = Math.floor((Math.random() * 10)+1);
+let num2 = Math.floor((Math.random() * 10)+1);
+let ans = num1 + num2;
+
+rl.question(`What is ${num1} + ${num2} ? \n`,
+(userInput)=>{
+    if(userInput.trim()== ans){// trim to get rid to trailing white space
+        rl.close();
+    }else{
+        rl.setPrompt('Incorrect response, plz try again\n');
+        rl.prompt();
+        rl.on('line',(userInput)=>{
+            if(userInput.trim() == ans){
+                rl.close();
+
+            }else{
+                rl.setPrompt(`Your ans of ${ userinput } is incorrect, try again`)
+                rl.prompt();
+            }
+        })
+    }
+    
+});
+
+rl.on('close',()=>{
+    console.log('Correct !!!');
+});
