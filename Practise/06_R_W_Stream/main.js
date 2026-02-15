38e386e4962995277a8a5a5532f6ae74223a286d
Sampat Sarkar <sampat7123@gmail.com>
2026-02-15 13:48:58 +0000

    Practised read&write stream

---
+++ Practise/06_R_W_Stream/main.js
@@ -0,0 +1,8 @@
+import fs from 'fs';
+const readStream = fs.createReadStream('./example.txt','utf-8');
+const writeStream = fs.createWriteStream('./example2.txt');
+readStream.on('data',(chunk)=>{
+    // console.log(chunk);
+    writeStream.write(chunk);
+    
+})
