c518c6bfd11b820fd8354e8f2a980c3a8db6d3a0
Sampat Sarkar <sampat7123@gmail.com>
2026-02-15 13:47:10 +0000

    Understood how to read and write stream

---
+++ 06_R_W_Streams/main.js
@@ -0,0 +1,8 @@
+import fs from 'fs';
+const readStream = fs.createReadStream('./example.txt','utf-8');
+const writeStream = fs.createWriteStream('example2.txt');
+readStream.on('data',(chunk)=>{
+    // writeStream.write(chunk);
+    console.log(chunk);
+    
+})
